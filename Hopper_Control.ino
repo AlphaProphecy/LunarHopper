beb49bad90bf26f85c807138e58fc18b421d9249
AlphaProphecy <srowbotham@me.com>
2019-11-22 19:34:46 +0000

    Inittal Commit to Personal

---
+++ Hopper_Control.ino
@@ -0,0 +1,423 @@
+/*-------------------------------
+
+Lunar Hopper Control System Code
+
+Written by: Marian Daogaru, mn2g12
+
+Updated by: Samuel Rowbotham      sjr1g18
+            Jack Tyler            jt6g15
+            Duncan Hamill         dh2g15
+            Boateng Opoku-Yeboah  boy1g15
+            Shane Lawson          sdl1n17
+---------------------------------
+*/
+
+#include "Hopper_Control_Classes.cpp"
+
+/*
+ * DECLERATIONS
+ */
+
+/* PIN NUMBERS
+ * Set pins for various input and output devices on arduino. e.g.- gyro, accelerometer,pressure transducer and solenoid valves.
+ */
+// Sensors (INPUTS)
+const int PRES_TRANS_PIN = A4;  // Input from Pressure Transducer [ANALOG]
+const int GYRO_PIN_X = A0;      // x-axis Gyro is connected to analog pin
+const int GYRO_PIN_Y = A5;      // y-axis gyro
+const int ACCEL_PIN_X = A1;     // x-axis accelerometer pin
+const int ACCEL_PIN_Y = A2;     // y-axis accelerometer pin
+const int ACCEL_PIN_Z = A3;     // z-axis accelerometer pin
+
+// Actuators (OUTPUTS)
+const int ACT_PIN_X_POS = 6;    // ACS valve positive x_axis
+const int ACT_PIN_X_NEG = 5;    // ACS valve negative x_axis
+const int ACT_PIN_Y_POS = 4;    // ACS valve positive y_axis
+const int ACT_PIN_Y_NEG = 3;    // ACS valve negative y_axis
+const int SOL_PIN_VENT = 12;    // Output to VENT solenoid control (SV2)
+const int SOL_PIN_PRES = 10;    // Output to PRESSURE solenoid control (SV1)
+const int SOL_PIN_OX = 11;      // Output to OX solenoid control(SV3)
+
+// Control pins (INPUTS)
+const int START_PIN = 30;
+const int ABORT_PIN = 2;
+const int DISARM_PIN = 1; // New pin to be added possibly
+const int THROTTLE_PIN = A6;
+
+// Valve constants for readability
+const int VALVE_OPEN = HIGH;
+const int VALVE_CLOSE = LOW;
+
+/*
+ * ACS
+ */
+
+// ACS configuration
+unsigned long timeZG = 0;                      // Variable to record the start time of zeroing the gyros
+unsigned long timeAcs = 0;                     // Variable used to record the start time of each ACS() loop
+bool acsInCalibration = false;                 // Tracking variable to detect if ACS subsystem is active
+bool acsCalibrated = false;                    // Tracking variable to record if ACS system has been acsCalibrated
+bool midCycle = false;                         // Tracking variable to record if ACS system is mid Cycle
+
+/*
+ *  Main Engine
+ */
+// General
+const float IGNITION_PRESSURE = 21.0; // bar (abs)
+bool pressurised = false; // Records if the engine has been pressurised
+bool ignited = false; // Records if the engine has been ignited
+
+// oxPulse specific
+int blipMode = 0;                            // Records the current mode of the oxPulse function (0: Ready to fire another blip, 1: Just fired a blip, 2: Just rested after a blip)
+int oxPulseNo = 0;                           // Records the number of oxygen pulses completed (In Ignition)
+unsigned long timePulse = 0;                 // Records the start time of the last ox blip
+
+
+/*
+ * System status
+ * Used to record the current state of the hopper
+ */
+bool startStatus = false;                   // Records the state of the start pin to start the flight
+bool disarmStatus = false;                  // Stores if the hopper has been disarmed
+bool abortStatus = false;                   // Stores if the hopper has aborted
+double currentPressure = 0;                 // Current pressre of the pressure transducer
+
+// Initialisation of objects.
+Accelerometer accelerometer;                // The accelerometers
+Gyro gyro;                                  // The gyros
+/*
+ * CODE
+ */
+
+// Setup Loop
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting serial process...");
+  pinMode(START_PIN, INPUT);      // Defines the Start pin as an INPUT
+  pinMode(ABORT_PIN, INPUT);      // Defines the Abort pin as an INPUT
+  pinMode(DISARM_PIN, INPUT);     // Defines the Disarm pin as an INPUT
+  pinMode(THROTTLE_PIN, INPUT);   // Defines the Throttle as an INPUT
+  pinMode(PRES_TRANS_PIN, INPUT); // Defines the Pressure Transducer reading as an INPUT
+  pinMode(GYRO_PIN_X, INPUT);     // Defines the Gyro X reading as an INPUT
+  pinMode(GYRO_PIN_Y, INPUT);     // Defines the Gyro Y reading as an INPUT
+  pinMode(ACCEL_PIN_X, INPUT);    // Defines the Accelerometer X reading as an INPUT
+  pinMode(ACCEL_PIN_Y, INPUT);    // Defines the Accelerometer Y reading as an INPUT
+  pinMode(ACCEL_PIN_Z, INPUT);    // Defines the Accelerometer Z reading as an INPUT
+  pinMode(ACT_PIN_X_POS, OUTPUT); // Defines the ACT positive X as an OUTPUT
+  pinMode(ACT_PIN_X_NEG, OUTPUT); // Defines the ACT negative X as an OUTPUT
+  pinMode(ACT_PIN_Y_POS, OUTPUT); // Defines the ACT positive Y as an OUTPUT
+  pinMode(ACT_PIN_Y_NEG, OUTPUT); // Defines the ACT negative Y as an OUTPUT
+  pinMode(SOL_PIN_VENT, OUTPUT);  // Defines the signal to Vent control as an OUTPUT
+  pinMode(SOL_PIN_PRES, OUTPUT);  // Defines the signal to Pressure control as an OUTPUT
+  pinMode(SOL_PIN_OX, OUTPUT);    // Defines the signal to OX control as an OUTPUT
+
+  accelerometer.setPins(ACCEL_PIN_X,ACCEL_PIN_Y,ACCEL_PIN_Z);
+  gyro.setPins(GYRO_PIN_X,GYRO_PIN_Y);
+}
+
+/*
+ * Main execution loop
+ */
+
+void loop() {
+  Serial.print("In loop:   ");
+  Serial.println(timeZG);
+
+  if (abortStatus == false) {
+    Serial.println("ABORT status = false");
+    if (disarmStatus == false) {
+      if (startStatus == true) {
+        Serial.println("Start = true");
+
+        if (acsCalibrated == false) {
+          Serial.println("Performing calibration...");
+          acsCalibration();
+
+        } else if (ignited == false) {
+          Serial.print("oxPulseNo = ");
+          Serial.println(oxPulseNo);
+          oxPulse();
+          
+        } else {
+          Serial.println("acs...");
+          acs();
+          Serial.println("throttle...");
+          throttle();
+
+        }
+      } else {
+        startStatus = digitalRead(START_PIN);
+        Serial.println("Reading start pin...");
+
+      }
+      if (disarmStatus == false){ // Prevents disarm status from being overwritten
+        disarmStatus = digitalRead(DISARM_PIN);
+
+      }
+      Serial.print("disarm status = ");
+      Serial.println(disarmStatus);
+    } else {
+      disarm();
+
+    }
+  } else {
+    abort();
+
+  }
+  if (abortStatus == false){ // Prevents abort status from being overwritten
+      abortStatus = digitalRead(ABORT_PIN);
+
+  }
+  Serial.print("abort status = ");
+  Serial.println(abortStatus);
+}
+  
+
+/*
+ * UPDATED TO LATEST VERSION 20/11/18 by Samuel Rowbotham
+ *  Not Verified through system tests/simulations.
+ *  Will slowly disarm the vehicle by having a gap between meco and ventAct
+ */
+void disarm() {
+  const int ACT_VENT_DELAY = 5000; // 5 seconds between engine cut off and venting of act 
+  disarmStatus = true;
+  
+  meco();
+
+  delay(ACT_VENT_DELAY);
+  ventAct();
+}
+
+/*
+ * UPDATED TO LATEST VERSION 20/11/2018 by Samuel Rowbotham
+ * Not Verified through system tests/simulations.
+ * Instantly shutsoff the engine and vents the ACT
+   The codes were therefore tested using the simulation function in the Xojo App which simulates the MECO remote being on/off and the hopper being armed/disarmed.
+ */
+
+void abort() { // Rename to abort for visual
+  Serial.println("ABORTING");
+  abortStatus = true;
+  
+  meco();
+  ventAct();
+}
+
+// Vents the ACS
+void ventAct() {
+  Serial.println("Venting ACT");
+  digitalWrite(ACT_PIN_X_POS, VALVE_OPEN);
+  digitalWrite(ACT_PIN_X_NEG, VALVE_OPEN);
+  digitalWrite(ACT_PIN_Y_POS, VALVE_OPEN);
+  digitalWrite(ACT_PIN_Y_NEG, VALVE_OPEN);
+}
+
+void meco() {
+  digitalWrite(SOL_PIN_OX, VALVE_CLOSE);
+  digitalWrite(SOL_PIN_PRES, VALVE_CLOSE);
+  digitalWrite(SOL_PIN_VENT, VALVE_OPEN);
+}
+
+/*acsCheck()--checks the commands related to the ACS and runs the appropriate routine.
+ *-------------Uses sub functions- ACS and acsCalibration
+ *-------------Also performs tasks relating to venting and ACT test.
+ *acsCalibration()-- Performs calibration for given calibration time
+ *acs()--The main function housing the control law for the hopper.
+ *-------Calculates current angles from gyro and accelerometers, applies complementary filtering
+ *-------Uses the control law to regulate the ACT solenoid valves
+ *
+ *
+ *Original Author: Zhihan Ma
+ *Modified, updated and verified by Achal Mittal
+ *NOT to be changed without approval from both Zhihan Ma and Achal Mittal
+ */
+
+void acsCalibration() {
+  const unsigned long  CALIBRATION_TIME = 40000; // Time used to zero the gyro
+
+  if (acsInCalibration == false){
+    acsCalibrated = false;
+    accelerometer.reset();
+    gyro.reset();
+    acsInCalibration = true;
+    timeZG = millis();      //start zeroing time
+    Serial.print("Checking, timeZG = ");
+    Serial.println(timeZG);
+  }
+  
+  if (timeZG + CALIBRATION_TIME > millis()) { // If calibration time is not over
+    accelerometer.addCalibrationValue();
+    gyro.addCalibrationValue();
+    Serial.print("Still calibrating at elapsed time: ");
+    Serial.println(timeZG + CALIBRATION_TIME-millis());
+
+  } else {
+    if (acsCalibrated == false) {  // If calibration time has just finished
+      accelerometer.finaliseCalibration(); // Calculate the zero voltages for the accelerometer
+      gyro.finaliseCalibration(); // Calculate the zero voltage for the gyroscope
+      acsCalibrated = true;      // Record calibration has finished
+    }
+  }
+}
+
+// Calculates the angle of the hopper and activates the ACT in response. If the angle is greter then MAX_ANGLE it abort's.
+void acs() {
+  const unsigned long TIME_INTERVAL = 25; // Time in milliseconds between each adjustment of the ACS system
+  const float CFF_VECTOR_GYRO = 0.90; // Complementary filter- choose what percentage of gyro angle you want in the final angle.
+  const float CFF_VECTOR_ACCEL = 0.10; // Complementary filter- choose what percentage of accelerometer angle you want in the final angle.
+  const float CONTROL_LAW_GAIN = 0.5; // Gain of control law
+
+  if (midCycle == false) { // If midCycle starting for first time or has just finished
+    midCycle = true; // Record the midCycle is starting
+    timeAcs = millis(); // Record the start time of the midCycle
+    gyro.setTimeAcs(timeAcs);
+
+  } else if (timeAcs + TIME_INTERVAL < millis()) { // If time interval has passed
+    midCycle = false;
+    
+    // Calculate angular rates and integrate for angle
+    accelerometer.calcAngles();
+    gyro.calcAngles();
+
+    float currentAngleX = CFF_VECTOR_GYRO * gyro.getAngleX() + CFF_VECTOR_ACCEL * accelerometer.getAngleX(); // Apply complementary filtering in z axis
+    float currentAngleY = CFF_VECTOR_GYRO * gyro.getAngleY() + CFF_VECTOR_ACCEL * accelerometer.getAngleY(); // Apply complementary filtering in y axis
+
+    float nextAngleX = currentAngleX + CONTROL_LAW_GAIN * gyro.getRateX();
+    float nextAngleY = currentAngleY + CONTROL_LAW_GAIN * gyro.getRateY();
+
+    acsThrustControl(ACT_PIN_X_POS, ACT_PIN_X_NEG, nextAngleX, "X");
+    acsThrustControl(ACT_PIN_Y_POS, ACT_PIN_Y_NEG, nextAngleY, "Y");
+    
+  }
+  // delay(10);
+}
+
+void acsThrustControl(int PIN_POS, int PIN_NEG, float nextAngle, char dir) {
+  const int MAX_ANGLE = 15; // This angle will trigger abort
+  const float CONTROL_LAW_DEADBAND = 0.1; // Threshold for control law (deadband) #maybe this is a bit too low, might require some changes
+
+  Serial.print("nextAngle");
+  Serial.print(dir);
+  Serial.print(" = ");
+  Serial.println(nextAngle);
+  if (abs(nextAngle) < MAX_ANGLE) {
+    if (abs(nextAngle) >= CONTROL_LAW_DEADBAND) {
+      if (nextAngle > 0) {    // Control law
+        digitalWrite(PIN_POS, VALVE_OPEN);
+        digitalWrite(PIN_NEG, VALVE_CLOSE);
+
+      } else if (nextAngle < 0) {
+        digitalWrite(PIN_POS, VALVE_CLOSE);
+        digitalWrite(PIN_NEG, VALVE_OPEN);
+      
+      }
+    } else {
+      digitalWrite(PIN_POS, VALVE_CLOSE);
+      digitalWrite(PIN_NEG, VALVE_CLOSE);
+    }
+  } else {
+    abort();
+  }
+}
+
+// Blips the OX to warm up the catalyst bed (Effective igniton)
+void oxPulse() {
+  const unsigned long BLIP_DURATION = 300; // Time in milliseconds the ox pulse lasts
+  const unsigned long BLIP_REST_DURATION = 5000; // Duration for which the system will wait for heat to dissipare in the fuel
+  const int OX_NO_PULSES = 3;
+
+  pressurisation();
+
+  if (pressurised == true) {
+    if (blipMode == 0) { // Ready to fire another blip
+      timePulse = millis();
+      digitalWrite(SOL_PIN_OX, VALVE_OPEN);
+      blipMode = 1;
+
+    } else if (blipMode == 1 && timePulse + BLIP_DURATION < millis()) { // Just fired a blip
+      digitalWrite(SOL_PIN_OX, VALVE_CLOSE);
+      blipMode = 2;
+      timePulse = millis();
+
+    } else if (blipMode == 2 && timePulse + BLIP_REST_DURATION < millis()) { // Just rested after a blip
+      blipMode = 0;
+      oxPulseNo++;
+    }
+  }
+  if (oxPulseNo >= OX_NO_PULSES) {
+    ignited = true;
+  }
+}
+
+// Pressurises the solonoid prior to oxPulse (ignition)
+void pressurisation() {
+  currentPressure = getPressure();
+
+  Serial.print("Current pressure = ");
+  Serial.println(currentPressure);
+
+  if (currentPressure < IGNITION_PRESSURE) {
+    digitalWrite(SOL_PIN_PRES, VALVE_OPEN);
+    pressurised = false;
+
+  } else {
+    digitalWrite(SOL_PIN_PRES, VALVE_CLOSE);
+    pressurised = true;
+  }
+}
+
+// Throttle control of the vehicle (controled manually)
+void throttle() {
+  currentPressure = getPressure();
+
+  float throttlePressure = ((analogRead(THROTTLE_PIN) / 1023 * 21 )) + 19;
+  Serial.print("Throttle pressure = ");
+  Serial.println(throttlePressure);
+  Serial.print("Current pressure = ");
+  Serial.println(currentPressure);
+
+  if (currentPressure < IGNITION_PRESSURE ) {
+    digitalWrite(SOL_PIN_PRES, VALVE_OPEN);
+    digitalWrite(SOL_PIN_OX, VALVE_CLOSE);
+    Serial.println("current < ig");
+
+  } else if (throttlePressure >= currentPressure && currentPressure > IGNITION_PRESSURE) {
+    digitalWrite(SOL_PIN_PRES, VALVE_OPEN);
+    digitalWrite(SOL_PIN_OX, VALVE_OPEN);
+    Serial.println("throttle >= current && current > ig");
+
+  } else if (throttlePressure < IGNITION_PRESSURE && IGNITION_PRESSURE < currentPressure) {
+    digitalWrite(SOL_PIN_PRES, VALVE_CLOSE);
+    digitalWrite(SOL_PIN_OX, VALVE_CLOSE);
+    Serial.println("throttle < ig < current");
+
+  } else if (throttlePressure < currentPressure && IGNITION_PRESSURE < currentPressure && IGNITION_PRESSURE < throttlePressure) {
+    digitalWrite(SOL_PIN_PRES, VALVE_CLOSE);
+    digitalWrite(SOL_PIN_OX, VALVE_OPEN);
+    Serial.println("ig < throttle < current");
+
+  }
+}
+
+// Returns the pressure in bars of the Pressure Transducer
+double getPressure() {
+  int voltage = analogRead(PRES_TRANS_PIN);
+  return voltage * (40 / 1023); //int * (bar/int) returns: bar
+}
+
+// Not used currently resets all values
+void reset() {
+  timeZG = 0;
+  acsInCalibration = false;
+  acsCalibrated = false;
+  midCycle = false;
+  timeAcs = 0;
+  blipMode = 0;
+  oxPulseNo = 0;
+  timePulse = 0;
+  currentPressure = 0;
+  pressurised = false;
+  startStatus = false;
+  disarmStatus = false;
+  abortStatus = false;
+}
---
+++ Hopper_Control_Classes.cpp
@@ -0,0 +1,193 @@
+#include "Arduino.h"
+// Classes
+class Accelerometer {
+  public:
+
+  	// Sets the pins of the accelerometer
+    setPins(int pinX, int pinY, int pinZ) {
+      PIN_X = pinX;
+      PIN_Y = pinY;
+      PIN_Z = pinZ;
+    }
+
+    // Calculates the current angle of the accelerometer
+    void calcAngles() {
+      const float ACCEL_SENSITIVITY = 0.8;
+      float zeroVoltZ = 1.775;
+
+      float x_g = ((analogRead(PIN_X) * VCC / 1023.00) - zeroVoltX) / ACCEL_SENSITIVITY;
+      float y_g = ((analogRead(PIN_Y) * VCC / 1023.00) - zeroVoltY) / ACCEL_SENSITIVITY;
+      float z_g = ((analogRead(PIN_Z) * VCC / 1023.00) - zeroVoltZ) / ACCEL_SENSITIVITY;
+      float c_g = sqrt(sq(y_g) + sq(z_g));  // Calculate the gravity using y and z axis
+      float d_g = sqrt(sq(x_g) + sq(z_g));  // Same as above using x and y axis
+      float accAngleInRadiansX = -atan2 (x_g, c_g);
+      float accAngleInRadiansY = -atan2 (y_g, d_g);
+      angleX = accAngleInRadiansX * 180 / M_PI;  // Get x-angle from accelerometer in degrees
+      angleY = accAngleInRadiansY * 180 / M_PI;  // Get y-angle from accelerometer in degrees
+    }
+
+
+    // Resets the accelerometer
+    void reset() {
+   	  calibrated = false;
+      acquisitionsCount = 0;
+      voltageSumX = 0;
+      voltageSumY = 0;
+    }
+
+    // Calculate the final variables
+    void finaliseCalibration() {
+      zeroVoltX = (voltageSumX * VCC) / (acquisitionsCount * 1023);
+      zeroVoltY = (voltageSumY * VCC) / (acquisitionsCount * 1023);
+      calibrated = true;
+    }
+
+    // Add one set of calibration numbers
+    void addCalibrationValue() {
+      voltageSumX += analogRead(PIN_X);
+      voltageSumY += analogRead(PIN_Y);
+      acquisitionsCount++;
+    }
+
+    // Getter for calibrated
+    bool isCalibrated() {
+      return calibrated;
+    }
+
+    // Getter for angleX
+    float getAngleX() {
+      return angleX;
+    }
+
+    // Getter for angleY
+    float getAngleY() {
+      return angleY;
+    }
+
+  private:
+    const float VCC = 5.0; // Stores the base voltage of the VCC
+
+    int PIN_X; // Stores the accelerometer's X pin
+    int PIN_Y; // Stores the accelerometer's Y pin
+    int PIN_Z; // Stores the accelerometer's Z pin
+
+    bool calibrated = false; // Stores if the accelerometer is calibrated
+
+    int acquisitionsCount; // Number of calibration values taken
+
+    float voltageSumX; // Sum of accelerometers X calibration Values
+    float voltageSumY; // Sum of accelerometers Y calibration Values
+
+    float zeroVoltX; // Calibrated zero voltage of the accelerometer X
+    float zeroVoltY; // Calibrated zero voltage of the accelerometer Y
+    
+    float angleX; // Accelerometers current angle in the x axis
+    float angleY; // Accelerometers current angle in the Y axis
+};
+
+
+class Gyro {
+  public:
+
+    // Sets the pins of the gyro
+    setPins(int pinX, int pinY) {
+      PIN_X = pinX;
+      PIN_Y = pinY;
+    }
+
+    // Calculates the current angles of the gyros and their rate of change
+    void calcAngles() {
+      const float GYRO_SENSITIVITY = 0.04;
+
+      int samplingCount = 0; 
+      float sumX = 0.0; 
+      float sumY = 0.0;
+      // Get reading ten times-over sampling to reduce noise
+      for (samplingCount = 0;samplingCount < 10; samplingCount++) {
+        sumX += analogRead(PIN_X); // Add current Z gyro voltage to running total
+        sumY += analogRead(PIN_Y);
+      }
+
+      //
+      // Below formula is based on the orientaiton that the gyro is mounted. Need to be careful when mounting
+      rateX = ( (sumX * VCC) / (samplingCount * 1023.) - zeroVoltX) / GYRO_SENSITIVITY * (1.0); // Calculate if any movement has occured in the X axis and correct angle (change sign due to how the gyro is mounted on the frame)
+      rateY = ( (sumY * VCC) / (samplingCount * 1023.) - zeroVoltY) / GYRO_SENSITIVITY * (1.0); // Same for y axis
+      //
+      //
+
+      angleX = rateX * (millis() - timeAcs + 15.00) * 0.001;  // Get x angle from x-axis gyro
+      angleY = rateY * (millis() - timeAcs + 15.00) * 0.001;  // Get y angle from y-axis gyro
+    }
+
+    // Resets the gyro
+    void reset() {
+      calibrated = false;
+      acquisitionsCount = 0;
+      voltageSumX = 0;
+      voltageSumY = 0;
+    }
+
+    // Calibrates the zeroVoltage of its X and Y angles
+    void finaliseCalibration() {
+      zeroVoltX = (voltageSumX * VCC) / (acquisitionsCount * 1023);
+      zeroVoltY = (voltageSumY * VCC) / (acquisitionsCount * 1023);
+      calibrated = true;
+    }
+
+    // Adds a calibration value to the gyro's sum
+    void addCalibrationValue() {
+      voltageSumX += analogRead(PIN_X);
+      voltageSumY += analogRead(PIN_Y);
+      acquisitionsCount++;
+    }
+
+    // Getter for angleX
+    float getAngleX() {
+      return angleX;
+    }
+
+    // Getter for angleY
+    float getAngleY() {
+      return angleY;
+    }
+
+    // Getter for rateX
+    float getRateX(){
+      return rateX;
+    }
+
+    // Getter for rateY
+    float getRateY(){
+      return rateY;
+    }
+    
+    // Sets the last time the ACS corrected the hoppers orientation
+    void setTimeAcs(unsigned long time){
+      timeAcs = time;
+    }
+
+  private:
+
+    const float VCC = 5.0; // Stores the base voltage of the VCC
+        
+    int PIN_X; // Gyro X pin
+    int PIN_Y; // Gyro Y pin
+
+    bool calibrated = false; // Stores if the gyro is calibrated
+
+    int acquisitionsCount; // Number of calibration values taken
+
+    float voltageSumX; // Sum of gyro X calibration Values
+    float voltageSumY; // Sum of gyro Y calibration Values
+
+    float zeroVoltX; // Calibrated zero voltage of the gyro X
+    float zeroVoltY; // Calibrated zero voltage of the gyro Y
+    
+    float angleX; // Gyros current angle in the x axis
+    float angleY; // Gyros current angle in the Y axis
+
+    float rateX; // Gyros current rate of change in the x axis
+    float rateY; // Gyros current rate of change in the y axis
+
+    unsigned long timeAcs = 0; // The last time the ACS corrected the hoppers orientation
+};
